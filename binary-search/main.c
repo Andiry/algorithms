f86883213463b4393fcefa8df3fffb84f976b83c
Andiry Xu <andiry.xu@gmail.com>
2013-09-07 13:06:07 -0700

    binary search implementation

--- binary-search/main.c
+++ binary-search/main.c
@@ -3,13 +3,14 @@
 
 #include"../library/array.h"
 #include"../library/sort.h"
+#include"../library/search.h"
 
 int main(int argc, char **argv)
 {
 	int *a, key, location;
 
 	int length = 12;
-	int seed;
+	int seed, ret;
 	int range = 256;
 
 	if (argc < 2)
@@ -18,6 +19,8 @@ int main(int argc, char **argv)
 		seed = atoi(argv[1]);
 
 	a = format_random_array(length, seed, range);
+	if (!a)
+		goto out;
 
 	print_array(a, length);
 	key = a[0];
@@ -25,13 +28,17 @@ int main(int argc, char **argv)
 	heap_init(a, length, 1);
 	print_array(a, length);
 
-	heap_sort(&a, length, 1);
+	ret = heap_sort(&a, length, 1);
+	if (ret)
+		goto out;
+
 	print_array(a, length);
-//	location = binary_search(a, key);
+	location = binary_search(a, length, key);
 
-	printf("%d is at %d\n", key, location);
+	if (location >= 0)
+		printf("%d is at %d\n", key, location);
 
+out:
 	free_array(a);
-
-	return 0;
+	return ret;
 }
