bb9b9d6af7d0ae793b3b97e69356191d0a46532e
Andiry Xu <andiry.xu@gmail.com>
2013-09-07 21:02:19 -0700

    Print LCS array and LCS string

--- LCS/main.c
+++ LCS/main.c
@@ -11,6 +11,61 @@
 
 #define MAX(a, b) ((a > b) ? (a) : (b))
 
+void print_lcs(int **array, char *a, char *b, int length)
+{
+	char *c = malloc(length + 1);
+	int lena = strlen(a);
+	int lenb = strlen(b);
+	int i, j;
+
+	c[length] = '\0';
+
+	i = lena;
+	j = lenb;
+
+	while(length) {
+		if (array[i][j] == array[i - 1][j - 1] + 1 &&
+				array[i][j] > array[i - 1][j] &&
+				array[i][j] > array[i][j - 1]) {
+			c[length - 1] = a[i - 1];
+			i--;
+			j--;
+			length--;
+		} else if (array[i][j] == array[i][j - 1]) {
+			j--;
+		} else {
+			i--;
+		}
+	}
+
+	printf("LCS: %s\n", c);
+	free(c);
+}
+
+void print_lcs1(int **array, char *a, char *b, int length)
+{
+	int lena = strlen(a) + 2;
+	int lenb = strlen(b) + 2;
+	int i, j;
+
+	for (i = 0; i< lena; i++) {
+		for (j = 0; j < lenb; j++) {
+			if (i == 0 && j <= 1)
+				printf("\t");
+			else if (i == 0)
+				printf("%c\t", b[j - 2]);
+			else if (i == 1 && j == 0) 
+				printf("\t");
+			else if (j == 0)
+				printf("%c\t", a[i - 2]);
+			else
+				printf("%d\t", array[i - 1][j - 1]);
+		}
+		printf("\n");
+	}
+				
+}
+
 int calculate_lcs(char *a, char *b)
 {
 	int lena = strlen(a);
@@ -37,6 +92,9 @@ int calculate_lcs(char *a, char *b)
  
 	ret = array[lena][lenb];
 
+	print_lcs1(array, a, b, ret);
+	print_lcs(array, a, b, ret);
+
 	free_2d_array(array, lena + 1, lenb + 1);
 
 	return ret;
@@ -44,8 +102,8 @@ int calculate_lcs(char *a, char *b)
 
 int main(int argc, char **argv)
 {
-	char *a = "BDECABA";
-	char *b = "ABDECBDAB";
+	char *a = "BDCABA";
+	char *b = "ABCBDAB";
 	int lcs;
 
 	lcs = calculate_lcs(a, b);
