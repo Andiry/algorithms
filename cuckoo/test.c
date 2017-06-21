c1fddc6acced62b39e7e53bbf5f66e0df1b72b2d
Andiry Xu <jix024@cs.ucsd.edu>
2017-06-20 23:44:15 -0700

    Cuckoo insert test

--- cuckoo/test.c
+++ cuckoo/test.c
@@ -5,6 +5,8 @@ int main(void) {
 	struct cuckoo * cuckoo;
 	char *key = "Hello World!";
 	unsigned long value = 0;
+	int i;
+	char str[10];
 
 	cuckoo = new_cuckoo();
 
@@ -17,6 +19,11 @@ int main(void) {
 	value = cuckoo_lookup(cuckoo, key, strlen(key));
 
 	printf("Key: %s, Value: %lu\n", key, value);
+
+	for (i = 1; i <= 4096; i++) {
+		sprintf(str, "%d", i);
+		cuckoo_insert(cuckoo, str, strlen(str), 123);
+	}
 
 	free_cuckoo(cuckoo);
 
